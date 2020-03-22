1d4233c45b9c048373c3e0bdb0a4f0d6b585f143
Joseph Avetsev <avetsev@gmail.com>
2020-03-22 20:03:10 +0200

    add custom exceptions, change interface hierarchy, add demo in main

--- hw3List/list-console/src/main/java/edu/dnu/fpm/pz/list/console/App.java
+++ hw3List/list-console/src/main/java/edu/dnu/fpm/pz/list/console/App.java
@@ -9,28 +9,27 @@ import edu.dnu.fpm.pz.list.interfaces.InvalidIndexException;
 public class App {
     public static void main(String[] args) {
         LinkedList<String> linkedList = new LinkedList<>();
-        listTest(linkedList);
-
         ArrayList<String> arrayList = new ArrayList<>();
-        listTest(arrayList);
-    }
 
-    public static void listTest(IList<String> list) {
         try {
-            list.addFirst("*");
-            list.addFirst("*");
-            list.addLast("!");
-            list.addFirst("*");
-            list.addFirst("*");
-            list.add(1, "hello :)");
-            list.change(1, "good bye :(");
-            list.remove(2);
-            list.removeFirst();
-            list.removeLast();
-            System.out.println(list.toString());
-
+            listTest(linkedList);
+            listTest(arrayList);
         } catch (InvalidIndexException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void listTest(IList<String> list) throws InvalidIndexException {
+        list.addFirst("*");
+        list.addFirst("*");
+        list.addLast("!");
+        list.addFirst("*");
+        list.addFirst("*");
+        list.add(1, "hello :)");
+        list.change(1, "good bye :(");
+        list.remove(2);
+        list.removeFirst();
+        list.removeLast();
+        System.out.println(list.toString());
     }
 }
