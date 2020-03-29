8a2df1b87dc66cf28c7211b851d6dcb201f5c297
Joseph Avetsev <avetsev@gmail.com>
2020-03-29 18:17:20 +0300

    processor test

--- hw4Processor/src/test/java/edu/dnu/fpm/pz/ProcessorTest.java
+++ hw4Processor/src/test/java/edu/dnu/fpm/pz/ProcessorTest.java
@@ -1,20 +1,90 @@
 package edu.dnu.fpm.pz;
 
+import edu.dnu.fpm.pz.assets.Consumer;
+import edu.dnu.fpm.pz.assets.Producer;
+import edu.dnu.fpm.pz.processor.Processor;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.mockito.*;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
-/**
- * Unit test for simple App.
- */
-public class ProcessorTest
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class ProcessorTest {
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Mock
+    Producer producer;
+
+    @Mock
+    Consumer consumer;
+
+    @Captor
+    private ArgumentCaptor<String> valueCaptor;
+
+    @InjectMocks
+    Processor processor = new Processor();
+
+    @Before
+    public void before() {
+        MockitoAnnotations.initMocks(this);
+    }
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testProduce() {
+        // Given.
+        String producedString = "Magic value";
+        Mockito.when(producer.produce()).thenReturn(producedString);
+
+        // When.
+        String result = producer.produce();
+
+        // Then.
+        Assert.assertNotEquals(producedString, result);
+        Assert.assertEquals(producedString, result);
+        Mockito.verify(producer).produce();
+    }
+
+    @Test
+    public void testConsume() {
+        // Given.
+        String producedString = "Magic value";
+
+        // When.
+        consumer.consume(producedString);
+
+        // Then.
+        Mockito.verify(consumer).consume(valueCaptor.capture());
+        String actual = valueCaptor.getValue();
+        assertEquals(producedString, actual);
+    }
+
+    @Test
+    public void testProcessWithException() {
+        // Given.
+        thrown.expect(IllegalStateException.class);
+
+        // When.
+        processor.process();
+
+        // Then.
+    }
+
+    @Test
+    public void testProcessWithoutException() {
+        // Given.
+        String producedString = "Magic value";
+        Mockito.when(producer.produce()).thenReturn(producedString);
+
+        // When.
+        processor.process();
+
+        // Then.
+        Mockito.verify(consumer).consume(valueCaptor.capture());
+        String actual = valueCaptor.getValue();
+        assertEquals(producedString, actual);
     }
 }
