2e3a84006321979708356df37972ae83f87efe30
Joseph Avetsev <avetsev@gmail.com>
2020-04-20 01:06:39 +0300

    configured liquibase.properties created crud interface created entities created tables

---
+++ hw6CRUD/CRUD-console/src/main/java/edu/dnu/fpm/pz/crud/console/App.java
@@ -0,0 +1,13 @@
+package edu.dnu.fpm.pz.crud.console;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
